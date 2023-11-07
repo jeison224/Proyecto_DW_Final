e9c0692c7e324266a24fde4c3a1e1d700e76ac07
JuanTB300405 <127706094+JuanTB300405@users.noreply.github.com>
2023-11-07 11:28:05 -0500

    feat: cambio en la carpeta del front, arreglo backend, otros cambios

--- back/schema/user.js
+++ back/schema/user.js
@@ -1,33 +1,32 @@
-const { hash } = require("bcrypt");
-const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
-
-    id:{type: Object},
-    usuario: {type:String, require:true, unique: true},
-    contraseña:{type:String, require:true},
-    nombre:{type:String, require:true},
+  id: { type: Object },
+  usuario: { type: String, require: true, unique: true },
+  contraseña: { type: String, require: true },
+  nombre: { type: String, require: true },
 });
-UserSchema.pre('save', function(next){
-    if(this.isModified('contraseña')|| this.isNew){
-        const document = this;
-        bcrypt.hash(document.contraseña,10,(err,hash)=>{
-            if(err){
-                next(err);
-            }else{
-                document.contraseña=hash;
-                next()
-            }
-        })
-    }else{
+UserSchema.pre("save", function (next) {
+  if (this.isModified("contraseña") || this.isNew) {
+    const document = this;
+    bcrypt.hash(document.contraseña, 10, (err, hash) => {
+      if (err) {
+        next(err);
+      } else {
+        document.contraseña = hash;
         next();
-    }
+      }
+    });
+  } else {
+    next();
+  }
 });
-UserSchema.methods.usuarioExist= async function(usuario){
-    const result= await mongoose.model('User').findOne({usuario});
-    return result > 0;
+UserSchema.methods.usuarioExist = async function (usuario) {
+  const result = await mongoose.model("User").find({ usuario });
+  return result.length > 0;
 };
-UserSchema.methods.compararContra= async function(contraseña,hash){
-    const same=await bcrypt.compare(contraseña,hash)
-    return same;
-}
-module.exports=mongoose.model("User",UserSchema);
+UserSchema.methods.compararContra = async function (contraseña, hash) {
+  const same = await bcrypt.compare(contraseña, hash);
+  return same;
+};
+module.exports = mongoose.model("User", UserSchema);
